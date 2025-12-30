35f64f3a075410038d48bf6c5aab4ca730021a81
Jonathan1324 <mohrjonathan09@gmail.com>
2025-12-30 17:58:13 +0100

    .

--- lct.rb
+++ lct.rb
@@ -1,24 +1,24 @@
 class Lct < Formula
-  desc "LCT"
+  desc "LCT is a toolset."
   homepage "https://github.com/Jonathan1324/lct"
   license "BSD-3-Clause"
   version "0.1.0-alpha.5"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/linked-assembly-macos-arm64.tar.gz"
-      sha256 "5989998488f51f4382465ab60d4f0aa6d67586c6233d2d93da003e09495c57cd"
+      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-macos-arm64.tar.gz"
+      sha256 "5989998488f51f4382465ab60d4f0aa6d67586c6233d2d93da003e09495c57cd" # TODO: Update Checksum
     else
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/linked-assembly-macos-x86_64.tar.gz"
-      sha256 "6ba08ddc402340ba912f2807f87a9b1a45bcb2d2df8b212f0a50e482438801d4"
+      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-macos-x86_64.tar.gz"
+      sha256 "6ba08ddc402340ba912f2807f87a9b1a45bcb2d2df8b212f0a50e482438801d4" # TODO: Update Checksum
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/linked-assembly-linux-arm64.tar.gz"
-      sha256 "b7e4a9d2669c46335f50ff5cdf36b9e05b3b5bc79ab130478ec5f841634b8aed"
+      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-linux-arm64.tar.gz"
+      sha256 "b7e4a9d2669c46335f50ff5cdf36b9e05b3b5bc79ab130478ec5f841634b8aed" # TODO: Update Checksum
     else
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/linked-assembly-linux-x86_64.tar.gz"
-      sha256 "14866965cca15ca2fe1c60aea8969dcfcaa116100abf2a7bdeca5222e6fb10a1"
+      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-linux-x86_64.tar.gz"
+      sha256 "14866965cca15ca2fe1c60aea8969dcfcaa116100abf2a7bdeca5222e6fb10a1" # TODO: Update Checksum
     end
   end
 
