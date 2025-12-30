4a4e049c6caa4bdfa33d4ac452a96d653a4cbdd1
Jonathan1324 <mohrjonathan09@gmail.com>
2025-12-30 18:28:04 +0100

    .

--- lct.rb
+++ lct.rb
@@ -4,27 +4,16 @@ class Lct < Formula
   license "BSD-3-Clause"
   version "0.1.0-alpha.5"
 
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-macos-arm64.tar.gz"
-      sha256 "57a3513abb18f8326757bc9173f111dc88ad050b2aa177ed922a6075649438de"
-    else
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-macos-x86_64.tar.gz"
-      sha256 "82484c8f27d1600ba2bf837bd827f3ae3411e85fb4de1897e1421b1b9cf5f340"
-    end
-  elsif OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-linux-arm64.tar.gz"
-      sha256 "541c51224b63c997cc41b2e7a7a6951451f7373e82601240a557ef1431ec3e23"
-    else
-      url "https://github.com/Jonathan1324/lct/releases/download/v#{version}/lct-linux-x86_64.tar.gz"
-      sha256 "eb0e2e9039024f732ec4651c1232939e25486223c9fa7c75a24ad8c641e1fb11"
-    end
-  end
+  url "https://github.com/Jonathan1324/lct/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "787dfb511388006363c5d58048ce68d1d1bc3f3a7664321b5e73097d5d3933a1"
+
+  depends_on "gcc" => :build
+  depends_on "make" => :build
+  depends_on "rust" => :build
+  depends_on "python@3.12" => :build
 
   def install
-    system "tar", "-xzf", cached_download, "-C", prefix
-    system "xattr", "-dr", "com.apple.quarantine", prefix if OS.mac?
+    system "python3", "-m", "ci.ci", "--no-test"
   end
 
   test do
