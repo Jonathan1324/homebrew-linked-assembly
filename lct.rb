9ae2154ddbe3d8559aa16dd79d6558893073874d
Jonathan1324 <mohrjonathan09@gmail.com>
2025-12-30 20:38:17 +0100

    update

--- lct.rb
+++ lct.rb
@@ -2,10 +2,10 @@ class Lct < Formula
   desc "LCT is a toolset."
   homepage "https://github.com/Jonathan1324/lct"
   license "BSD-3-Clause"
-  version "0.1.0-alpha.5"
+  version "0.1.0-alpha.5-dec.2"
 
   url "https://github.com/Jonathan1324/lct/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "787dfb511388006363c5d58048ce68d1d1bc3f3a7664321b5e73097d5d3933a1"
+  sha256 "329071e2cf201b836f55ab64c55901f01c52e2977a91747f55157cad2e1168a2"
 
   depends_on "gcc" => :build
   depends_on "make" => :build
